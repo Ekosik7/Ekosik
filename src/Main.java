260605f43b7ba5987b18d146494b52bdd83a2c15
Ekosik7 <zzh.eraly121206@gmail.com>
2024-12-21 11:49:33 +0500

    Made the example for the first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Person> students = new ArrayList<>();
+        Person person1 = new Person("Yeraly", "Zalel", 18, true);
+        System.out.println(person1.toString());
     }
 }
